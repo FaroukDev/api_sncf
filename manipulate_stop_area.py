a91b0d419306fdcfde22b0c5ef8a16a20e35c3c0
FaroukDev <fa.elayache1@gmail.com>
2021-01-21 16:40:59 +0100

    add method read to fetch json file stop areas

---
+++ manipulate_stop_area.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+r = requests.get('https://simplonline-v3-prod.s3.eu-west-3.amazonaws.com/media/file/txt/3fa48b7d-ce01-4268-8cbf-a3eecc8df7bb.txt')
+#print(r.json())
+
+with open('stop_areas.json',"r") as json_file:
+    data = json.load(json_file)
+
+print(data)
