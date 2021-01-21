55a8bb3e0b81c98c75d29c81a4a87a5fc055b7c0
FaroukDev <fa.elayache1@gmail.com>
2021-01-21 17:17:32 +0100

    import pprint and add to my json file

--- manipulate_stop_area.py
+++ manipulate_stop_area.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from pprint import pprint
+
 
 r = requests.get('https://simplonline-v3-prod.s3.eu-west-3.amazonaws.com/media/file/txt/3fa48b7d-ce01-4268-8cbf-a3eecc8df7bb.txt')
 #print(r.json())
@@ -7,4 +9,8 @@ r = requests.get('https://simplonline-v3-prod.s3.eu-west-3.amazonaws.com/media/f
 with open('stop_areas.json',"r") as json_file:
     data = json.load(json_file)
 
-print(data)
+#print(data)
+
+#print(json.dumps(data, indent=4))
+
+pprint(r.json())
