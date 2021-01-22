853b02416682db48016da3ba3cda0fa9a6907226
FaroukDev <fa.elayache1@gmail.com>
2021-01-22 16:44:44 +0100

    find url and id in json file

--- manipulate_stop_area.py
+++ manipulate_stop_area.py
@@ -1,16 +1,58 @@
+import pprint
 import json
 import requests
-from pprint import pprint
+import csv
+
+url = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
+headers = {"Authorization": "e3f2b3a6-caa9-47d7-98ee-1f67379e654b"}
+response = requests.get(url, headers=headers) #pop up for password
+raw_data = json.loads(response.text)
+
+print(response)
+print(type(raw_data))
+
+areas = raw_data["stop_areas"] #list
+
+print(type(areas))
+
+area = areas[2] #dict
+
+list_ids = []
+
+for loop_area in areas:
+    if type(loop_area) == dict:
+        if "id" in loop_area.keys(): #!!!!!!
+            local_id = loop_area["id"]
+            list_ids.append(local_id)
+        else:
+            print("Missing key id")
+    else:
+        print(f"Unexpected format {type(loop_area)}")
+
+print(len(list_ids))
 
 
-r = requests.get('https://simplonline-v3-prod.s3.eu-west-3.amazonaws.com/media/file/txt/3fa48b7d-ce01-4268-8cbf-a3eecc8df7bb.txt')
-#print(r.json())
+# print(type(area), area)
 
-with open('stop_areas.json',"r") as json_file:
-    data = json.load(json_file)
+# print(area.keys())
 
-#print(data)
+# print(area["id"])
 
-#print(json.dumps(data, indent=4))
 
-pprint(r.json())
+'''
+how to find url 
+'''
+
+list_link = []
+
+for loop_link in areas:
+    if type(loop_link) == dict:
+        if "links" in loop_link.keys():
+            local_link = loop_link["links"]
+            list_link.append(local_link)
+        else:
+            print("missing key links")
+    else:
+        print(f"Unexpected format {type(loop_link)}")
+    
+print(len(list_link))
