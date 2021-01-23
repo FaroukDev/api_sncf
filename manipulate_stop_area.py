966e4c4aff093dc484a9c8e27103aead356bd155
FaroukDev <fa.elayache1@gmail.com>
2021-01-23 12:46:49 +0100

    add name and id in data.csv

--- manipulate_stop_area.py
+++ manipulate_stop_area.py
@@ -2,6 +2,10 @@ import pprint
 import json
 import requests
 import csv
+import logging
+
+logging.basicConfig(filename='data.log', level=logging.DEBUG)
+logging.info("Script started")
 
 url = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
 headers = {"Authorization": "6df7dc97-1bdc-4539-af03-9d854dd236fe"}
@@ -20,6 +24,8 @@ area = areas[2] #dict
 list_ids = []
 list_name = []
 
+
+logging.info("find name and id in areas")
 for loop_area, loop_name in zip(areas, areas):
     if type(loop_area) == dict and type(loop_name) ==  dict:
         if "id" in loop_area.keys() and "name" in loop_name.keys(): #!!!!!!
@@ -45,7 +51,7 @@ print(list_ids, list_name)
 '''
 how to find url 
 '''
-
+logging.info("find url in areas json file")
 list_link = []
 
 for loop_link in areas:
@@ -63,6 +69,24 @@ for loop_link in areas:
 #print(area)
 
 '''
+transform in csv file
+'''
+logging.info("transform file in csv")
+fname = "data.csv"
+
+with open(fname, 'w') as csv_file:
+    csv_file = csv.writer(csv_file)
+    csv_file.writerow(["id","name"])
+    for item in areas:
+        csv_file.writerow([item['id'], item['name']])
+        
+        
+
+
+
+
+
+'''
 how to find name
 '''
 list_name = []
@@ -77,3 +101,5 @@ for loop_name in areas:
     else:
         print(f"Unexpected format {type(loop_name)}")
 #print(list_name)
+
+logging.info("script_ended")
