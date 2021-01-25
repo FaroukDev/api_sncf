260a1ca36b2b5d5ce2ffe5686b5c0cf136dd694f
FaroukDev <fa.elayache1@gmail.com>
2021-01-25 15:07:27 +0100

    add function

--- manipulate_stop_area.py
+++ manipulate_stop_area.py
@@ -1,4 +1,4 @@
-import pprint
+from pprint import pprint
 import json
 import requests
 import csv
@@ -9,44 +9,47 @@ logging.info("Script started")
 
 url = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
 headers = {"Authorization": "6df7dc97-1bdc-4539-af03-9d854dd236fe"}
-response = requests.get(url, headers=headers) #pop up for password
+response = requests.get(url, headers=headers)  # pop up for password
 raw_data = json.loads(response.text)
 
 print(response)
 print(type(raw_data))
 
-areas = raw_data["stop_areas"] #list
+areas = raw_data["stop_areas"]  # list
+
+pprint(areas)
 
 print(type(areas))
 
-area = areas[2] #dict
+area = areas[2]  # dict
 
 list_ids = []
 list_name = []
 
-
 logging.info("find name and id in areas")
-for loop_area, loop_name in zip(areas, areas):
-    if type(loop_area) == dict and type(loop_name) ==  dict:
-        if "id" in loop_area.keys() and "name" in loop_name.keys(): #!!!!!!
-            local_id = loop_area["id"]
-            local_name = loop_name["name"]
-            list_ids.append(local_id)
-            list_name.append(local_name)
-        else:
-            print("Missing key id")
-    else:
-        print(f"Unexpected format {type(loop_area)}")
 
+def fetchnameandid():
+    for loop_area, loop_name in zip(areas, areas):
+        if type(loop_area) == dict and type(loop_name) == dict:
+            if "id" in loop_area.keys() and "name" in loop_name.keys():  # !!!!!!
+                local_id = loop_area["id"]
+                local_name = loop_name["name"]
+                list_ids.append(local_id)
+                list_name.append(local_name)
+            else:
+                print("Missing key id")
+        else:
+            print(f"Unexpected format {type(loop_area)}")
+
+fetchnameandid()
 print(list_ids, list_name)
 
-
+print(len(list_ids))
 # print(type(area), area)
 
 #print(area.keys())
 
 # print(area["id"])
-
 
 '''
 how to find url 
@@ -54,19 +57,20 @@ how to find url
 logging.info("find url in areas json file")
 list_link = []
 
-for loop_link in areas:
-    if type(loop_link) == dict:
-        if "links" in loop_link.keys():
-            local_link = loop_link["links"]
-            list_link.append(local_link)
+def findurl():
+    for loop_link in areas:
+        if type(loop_link) == dict:
+            if "links" in loop_link.keys():
+                local_link = loop_link["links"]
+                list_link.append(local_link)
+            else:
+                print("missing key links")
         else:
-            print("missing key links")
-    else:
-        print(f"Unexpected format {type(loop_link)}")
-    
-#print(list_link)
+            print(f"Unexpected format {type(loop_link)}")
 
-#print(area)
+findurl()
+print(list_link)
+
 
 '''
 transform in csv file
@@ -74,29 +78,32 @@ transform in csv file
 logging.info("transform file in csv")
 fname = "data.csv"
 
-with open(fname, 'w') as csv_file:
-    csv_file = csv.writer(csv_file)
-    csv_file.writerow(["id","name"])
-    for item in areas:
-        csv_file.writerow([item['id'], item['name']])
+def transformincsv():
+    with open(fname, 'w') as csv_file:
+        csv_file = csv.writer(csv_file)
+        csv_file.writerow(["id","name"])
+        for item in areas:
+            csv_file.writerow([item['id'], item['name']])
         
-
+transformincsv()
 '''
 how to find name
 '''
 list_name = []
 
-for loop_name in areas:
-    if type(loop_name) == dict:
-        if "name" in loop_name.keys():
-            local_name = loop_name["name"]
-            list_name.append(local_name)
+def findname():
+    for loop_name in areas:
+        if type(loop_name) == dict:
+            if "name" in loop_name.keys():
+                local_name = loop_name["name"]
+                list_name.append(local_name)
+            else:
+                print("missing key name")
         else:
-            print("missing key name")
-    else:
-        print(f"Unexpected format {type(loop_name)}")
-#print(list_name)
+            print(f"Unexpected format {type(loop_name)}")
 
+findname()
+print(list_name)
 
 logging.info("fetch data info between paris and lyon")
 '''
@@ -123,7 +130,24 @@ number of stop areas between paris and lyon
 '''
 
 logging.info("fetch number of stop areas between paris and lyon")
-for data in raw_data["journeys"]:
-    print(len(data))
+#for data in raw_data["journeys"]:
+    #print(len(data))
+
+'''
+Combien de temps d'arrêts entre chacunes d'elles
+'''
+logging.info("how long does each stop have")
+
+URL = f"https://api.sncf.com/v1/coverage/sncf/journeys?from={paris}"
+headers = {"Authorization": "6df7dc97-1bdc-4539-af03-9d854dd236fe"}
+req = requests.get(URL, headers=headers)
+
+stop_data = req.json()
+
+for stop in stop_data:
+    pprint(stop)
+
+#list_between_eight_and_twenty = []
+
 
 logging.info("script_ended")
