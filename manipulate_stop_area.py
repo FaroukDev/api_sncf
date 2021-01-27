bfce259271ba4bb2c9e9ef3152738d3b755add47
FaroukDev <fa.elayache1@gmail.com>
2021-01-27 11:02:25 +0100

    add test in test.py

--- manipulate_stop_area.py
+++ manipulate_stop_area.py
@@ -5,148 +5,153 @@ import csv
 import logging
 
 
-class main:
-    logging.basicConfig(filename='data.log', level=logging.DEBUG)
-    logging.info("Script started")
 
-    url = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
-    headers = {"Authorization": "6df7dc97-1bdc-4539-af03-9d854dd236fe"}
+logging.basicConfig(filename='data.log', level=logging.DEBUG)
+logging.info("Script started")
 
-    def read_json(self, url, headers):  # read and saves json
-        response = requests.get(url, headers=headers)  # pop up for password
-        # raw_data = json.loads(response.text) #dict
-        with open('stop_areas.json', mode="w") as file:
-            json.dump(response.text, file)
-        # returns nothing, saves json
+url = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
+headers = {"Authorization": "6df7dc97-1bdc-4539-af03-9d854dd236fe"}
 
+def read_json():  # read and saves json
     response = requests.get(url, headers=headers)  # pop up for password
-    raw_data = json.loads(response.text)
+    # raw_data = json.loads(response.text) #dict
+    with open('stop_areas.json', mode="w") as file:
+        json.dump(response.text, file)
+    # returns nothing, saves json
 
-    print(response)
-    print(type(raw_data))
+response = requests.get(url, headers=headers)  # pop up for password
+raw_data = json.loads(response.text)
 
-    areas = raw_data["stop_areas"]  # list
+print(response)
+print(type(raw_data))
 
-    pprint(areas)
+areas = raw_data["stop_areas"]  # list
 
-    print(type(areas))
+pprint(areas)
 
-    # area = areas[2]  # dict
+print(type(areas))
 
-    logging.info("find name and id in areas")
+# area = areas[2]  # dict
 
+logging.info("find name and id in areas")
+
+
+
+def fetchnameandid():
     list_ids = []
     list_name = []
-
-    def fetchnameandid(self, areas, list_ids, list_name):
-        for loop_area, loop_name in zip(areas, areas):
-            if type(loop_area) == dict and type(loop_name) == dict:
-                if "id" in loop_area.keys() and "name" in loop_name.keys():  # !!!!!!
-                    local_id = loop_area["id"]
-                    local_name = loop_name["name"]
-                    list_ids.append(local_id)
-                    list_name.append(local_name)
-                else:
-                    print("Missing key id")
+    for loop_area, loop_name in zip(areas, areas):
+        if type(loop_area) == dict and type(loop_name) == dict:
+            if "id" in loop_area.keys() and "name" in loop_name.keys():  # !!!!!!
+                local_id = loop_area["id"]
+                local_name = loop_name["name"]
+                list_ids.append(local_id)
+                list_name.append(local_name)
             else:
-                print(f"Unexpected format {type(loop_area)}")
+                print("Missing key id")
+        else:
+            print(f"Unexpected format {type(loop_area)}")
+    return list_ids, list_name
+#print(list_ids, list_name)
 
-    #print(list_ids, list_name)
+# print(len(list_ids))
+# print(type(area), area)
 
-    # print(len(list_ids))
-    # print(type(area), area)
+# print(area.keys())
 
-    # print(area.keys())
+# print(area["id"])
 
-    # print(area["id"])
+'''
+how to find url 
+'''
+logging.info("find url in areas json file")
 
-    '''
-    how to find url 
-    '''
-    logging.info("find url in areas json file")
+
+def findurl():
     list_link = []
-
-    def findurl(self, areas, list_link):
-        for loop_link in areas:
-            if type(loop_link) == dict:
-                if "links" in loop_link.keys():
-                    local_link = loop_link["links"]
-                    list_link.append(local_link)
-                else:
-                    print("missing key links")
+    for loop_link in areas:
+        if type(loop_link) == dict:
+            if "links" in loop_link.keys():
+                local_link = loop_link["links"]
+                list_link.append(local_link)
             else:
-                print(f"Unexpected format {type(loop_link)}")
+                print("missing key links")
+        else:
+            print(f"Unexpected format {type(loop_link)}")
+    return list_link
+# findurl()
+#print("hello")
+#print(list_link)
 
-    # findurl()
-    print(list_link)
+'''
+transform in csv file
+'''
+logging.info("transform file in csv")
 
-    '''
-    transform in csv file
-    '''
-    logging.info("transform file in csv")
+
+def transform_in_csv():
     fname = "data.csv"
+    with open(fname, 'w') as csv_file:
+        csv_file = csv.writer(csv_file)
+        csv_file.writerow(["id", "name"])
+        for item in areas:
+            return  csv_file.writerow([item['id'], item['name']])
 
-    def transform_in_csv(self, fname, areas):
-        with open(fname, 'w') as csv_file:
-            csv_file = csv.writer(csv_file)
-            csv_file.writerow(["id", "name"])
-            for item in areas:
-                csv_file.writerow([item['id'], item['name']])
+# transform_in_csv()
+'''
+how to find name
+'''
 
-    # transform_in_csv()
-    '''
-    how to find name
-    '''
+
+def findname():
     list_name = []
-
-    def findname(self, areas, list_name):
-        for loop_name in areas:
-            if type(loop_name) == dict:
-                if "name" in loop_name.keys():
-                    local_name = loop_name["name"]
-                    list_name.append(local_name)
-                else:
-                    print("missing key name")
+    for loop_name in areas:
+        if type(loop_name) == dict:
+            if "name" in loop_name.keys():
+                local_name = loop_name["name"]
+                list_name.append(local_name)
             else:
-                print(f"Unexpected format {type(loop_name)}")
+                print("missing key name")
+        else:
+            print(f"Unexpected format {type(loop_name)}")
+    return list_name
+# findname()
+#print(list_name)
 
-    # findname()
-    print(list_name)
+logging.info("fetch data info between paris and lyon")
+'''
+fetch journey between paris and lyon
+'''
+paris = "stop_area:OCE:SA:87686006"
 
-    logging.info("fetch data info between paris and lyon")
-    '''
-    fetch journey between paris and lyon
-    '''
-    paris = "stop_area:OCE:SA:87686006"
+lyon = "stop_area:OCE:SA:87722025"
 
-    lyon = "stop_area:OCE:SA:87722025"
+URL = f"https://api.sncf.com/v1/journeys?from={paris}&to={lyon}"
 
-    URL = f"https://api.sncf.com/v1/journeys?from={paris}&to={lyon}"
+headers = {"Authorization": "6df7dc97-1bdc-4539-af03-9d854dd236fe"}
+req = requests.get(URL, headers=headers)
 
-    headers = {"Authorization": "6df7dc97-1bdc-4539-af03-9d854dd236fe"}
-    req = requests.get(URL, headers=headers)
+raw_data = req.json()
 
-    raw_data = req.json()
+for data in raw_data["journeys"]:
+    print(data)
 
-    for data in raw_data["journeys"]:
-        print(data)
+'''
+number of stop areas between paris and lyon
+'''
+print(len(raw_data['journeys'][0]['sections'][1]['stop_date_times']))
 
-    '''
-    number of stop areas between paris and lyon
-    '''
-    print(len(raw_data['journeys'][0]['sections'][1]['stop_date_times']))
+logging.info("fetch number of stop areas between paris and lyon")
+# for data in raw_data["journeys"]:
+# print(len(data))
 
-    logging.info("fetch number of stop areas between paris and lyon")
-    # for data in raw_data["journeys"]:
-    # print(len(data))
+'''
+Combien de temps d'arrêts entre chacunes d'elles
+'''
+logging.info("how long does each stop have")
 
-    '''
-    Combien de temps d'arrêts entre chacunes d'elles
-    '''
-    logging.info("how long does each stop have")
+stop_data = req.json()
 
-    stop_data = req.json()
+#list_between_eight_and_twenty = []
 
-    #list_between_eight_and_twenty = []
-
-    logging.info("script_ended")
+logging.info("script_ended")
--- test.py
+++ test.py
@@ -1,11 +1,29 @@
 import os
-from manipulate_stop_area import read_json
+from manipulate_stop_area import read_json, fetchnameandid,findurl, findname, transform_in_csv
 import unittest
+import requests
+import json
+
 
 class TestJsonInPath(unittest.TestCase):
     def test_check_file_exist(self):
         directory_path = './stop_areas.json' # somepath
         self.assertTrue(os.path.exists(directory_path))
-        
+
+    def test_find_id_and_name(self):
+        self.assertTrue(fetchnameandid())
+
+    def findlinks(self):
+        self.assertTrue(findurl())
+
+    def test_find_name(self):
+        self.assertTrue(findname())
+    
+    def test_transform_in_csv(self):
+        directory_path = './data.csv'
+        self.assertTrue(os.path.exists(directory_path))
+
 if __name__ == '__main__':
     unittest.main()
+
+    
